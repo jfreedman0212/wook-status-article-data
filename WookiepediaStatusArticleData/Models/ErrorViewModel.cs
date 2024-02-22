bfd5cd9762ddbbbb255f1ec7c73a0ac41a3f65ce
Josh Freedman <josh@slashpine.tech>
2024-02-22 12:48:26 -0500

    Generate project from template

---
+++ WookiepediaStatusArticleData/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WookiepediaStatusArticleData.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
