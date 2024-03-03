e553112e7d2c6304e80d9ff50861a02b3f48e81b
Josh Freedman <josh@slashpine.tech>
2024-03-03 10:37:52 -0500

    Add beginnings of nominator management

---
+++ WookiepediaStatusArticleData/Controllers/NominatorsController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WookiepediaStatusArticleData.Database;
+using WookiepediaStatusArticleData.Models.Nominators;
+using WookiepediaStatusArticleData.Nominations;
+
+namespace WookiepediaStatusArticleData.Controllers;
+
+[Route("nominators")]
+public class NominatorsController(WookiepediaDbContext db) : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        var nominators = await db.Set<Nominator>()
+            .Include(it => it.Attributes)
+            .OrderBy(it => it.Name)
+            .ToListAsync(cancellationToken);
+        
+        return View(new NominatorsViewModel { Nominators = nominators });
+    }
+
+    [HttpGet("edit/{id:int}")]
+    public async Task<IActionResult> EditForm([FromRoute] int id, CancellationToken cancellationToken)
+    {
+        var nominator = await db.Set<Nominator>()
+            .Include(it => it.Attributes)
+            .SingleOrDefaultAsync(it => it.Id == id, cancellationToken);
+
+        if (nominator == null) return NotFound();
+        
+        return View(NominatorForm.From(nominator));
+    }
+}
