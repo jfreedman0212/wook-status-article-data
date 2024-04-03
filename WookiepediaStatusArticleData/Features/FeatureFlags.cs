90af3587e89946a77ddc06a4c0cc91168452cf9b
Josh Freedman <josh@slashpine.tech>
2024-04-02 23:25:34 -0400

    Add functionality to create nominators + enable feature

--- WookiepediaStatusArticleData/Features/FeatureFlags.cs
+++
@@ -1,6 +0,0 @@
-namespace WookiepediaStatusArticleData.Features;
-
-public static class FeatureFlags
-{
-    public const string NominatorManagement = nameof(NominatorManagement);
-}
--- WookiepediaStatusArticleData/Models/Nominators/NominatorEditViewModel.cs
+++ WookiepediaStatusArticleData/Models/Nominators/NominatorEditViewModel.cs
@@ -6,6 +6,6 @@ public class NominatorEditViewModel
 {
     public int Id { get; init; }
     public required string Name { get; init; }
-    public required IList<NominatorAttributeType> Attributes { get; init; }
+    public required IList<NominatorAttributeType> Attributes { get; init; } = [];
     public required IList<NominatorAttributeType> AllowedAttributes { get; init; }
 }
