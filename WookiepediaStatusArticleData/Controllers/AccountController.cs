7a57941cef324a86dc6252917e33bf62a52f0276
Josh Freedman <josh@freedman.dev>
2024-07-10 12:34:20 -0400

    Revert "Add dynamic URI scheme for account controller"
    This reverts commit 77e33bde55ca8f5ac3ff249bdeddeca70c28c1d6.

--- WookiepediaStatusArticleData/Controllers/AccountController.cs
+++ WookiepediaStatusArticleData/Controllers/AccountController.cs
@@ -13,12 +13,9 @@ public class AccountController : Controller
     public async Task Login([FromQuery] string? returnUrl)
     {
         var properties = new LoginAuthenticationPropertiesBuilder()
-            .WithRedirectUri(returnUrl
-                             ?? Url.Action("Index", "Projects", null, Request.IsHttps ? "https" : "http")
-                             ?? throw new InvalidOperationException()
-            )
+            .WithRedirectUri((returnUrl ?? Url.Action("Index", "Projects")) ?? throw new InvalidOperationException())
             .Build();
-
+        
         await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, properties);
     }
 
@@ -27,12 +24,9 @@ public class AccountController : Controller
     public async Task Logout()
     {
         var properties = new LoginAuthenticationPropertiesBuilder()
-            .WithRedirectUri(
-                Url.Action("Index", "Home", null, Request.IsHttps ? "https" : "http")
-                ?? throw new InvalidOperationException()
-            )
+            .WithRedirectUri(Url.Action("Index", "Home") ?? throw new InvalidOperationException())
             .Build();
-
+        
         await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, properties);
         await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
     }
