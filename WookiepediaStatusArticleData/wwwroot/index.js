d5c1a0e3eabf03bd694c7a7e3868d39595c27491
Josh Freedman <josh@freedman.dev>
2024-07-10 12:34:20 -0400

    Fix some of the jank and add more robust error handling

--- WookiepediaStatusArticleData/wwwroot/index.js
+++ WookiepediaStatusArticleData/wwwroot/index.js
@@ -1,31 +1,61 @@
+import Toastify from 'https://cdn.jsdelivr.net/npm/toastify-js@1.12.0/+esm';
+
+window.addEventListener('htmx:load', function (event) {
+    const timeComponents = event.detail.elt.querySelectorAll('time');
+    timeComponents.forEach(function (time) {
+        const date = new Date(time.getAttribute('datetime'));
+        const formatter = new Intl.DateTimeFormat('en-US', { 
+            month: '2-digit',
+            day: '2-digit',
+            year: 'numeric'
+        });
+        time.textContent = formatter.format(date);
+    });
+});
+
+window.addEventListener('htmx:beforeSwap', function (event) {
+   event.detail.shouldSwap = event.detail.xhr.status === 200 || event.detail.xhr.status === 400; 
+});
+
 window.addEventListener('htmx:responseError', function (event) {
-    let message = 'Your request couldn\'t be processed. Try again later.';
-    let close = false;
-    let className = 'box bad';
+    // all of the below code is for error handling, so do nothing for successful requests or validation errors
+    if (event.detail.successful || event.detail.xhr.status === 400) return;
     
+    let message = 'Your request couldn\'t be processed. Try again later.';
+    let className = 'box bad';
+
     switch (event.detail.xhr.status) {
         case 401:
             message = 'You must log in to access that.';
-            close = true;
             className = 'box warn';
             break;
         case 403:
             message = 'You don\'t have permission to access that.';
-            close = true;
             className = 'box warn';
             break;
         case 404:
             message = 'The requested resource does not exist.';
-            close = true;
             className = 'box warn';
             break;
     }
     Toastify({
         text: message,
-        duration: 3000,
-        newWindow: true,
-        close: close,
+        duration: -1,
+        close: true,
         className: className,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+    }).showToast();
+});
+
+window.addEventListener('htmx:sendError', function () {
+    Toastify({
+        text: 'Your request could not be sent. This can happen if your internet connection isn\'t stable or if the application is down for maintenance. If your connection is stable, try again in a few moments.',
+        duration: -1,
+        newWindow: true,
+        close: true,
+        className: 'box bad',
         gravity: "top", // `top` or `bottom`
         position: "left", // `left`, `center` or `right`
         stopOnFocus: true, // Prevents dismissing of toast on hover
