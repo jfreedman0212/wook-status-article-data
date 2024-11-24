54eb5304bf719f16fdad9cf1bc2ef7fe59cce984
Josh Freedman <josh@freedman.dev>
2024-11-24 06:49:46 -0500

    Force use of https for requests w/o ASPNETCORE_URLS
    Maybe this will work???
    
    This reverts commit 3000fcc6ef5f030a0731cf735bd1b634038bd6b8.

--- WookiepediaStatusArticleData/Startup.cs
+++ WookiepediaStatusArticleData/Startup.cs
@@ -52,6 +52,12 @@ public class Startup(IConfiguration configuration, IWebHostEnvironment environme
             app.UseExceptionHandler("/home/error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
+            
+            app.Use((context, next) =>
+            {
+                context.Request.Scheme = "https";
+                return next();
+            });
         }
 
         app.UseForwardedHeaders(
