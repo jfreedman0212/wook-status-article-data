6f210df0e6de71b56908c418faf136f6680049e0
Josh Freedman <josh@freedman.dev>
2024-11-30 10:41:03 -0500

    Reformat file

--- WookiepediaStatusArticleData/Services/Nominators/AttributeTimeline/TimelineParser.cs
+++ WookiepediaStatusArticleData/Services/Nominators/AttributeTimeline/TimelineParser.cs
@@ -17,17 +17,17 @@ public class TimelineParser : IDisposable
             .Where(token => token.Type != TimelineTokenType.Comment)
             .GetEnumerator();
     }
-    
+
     public IEnumerable<TimelineDirective> Parse()
     {
         // each iteration of this loop should represent a command, which must be the start of a line.
         // assume the top of the loop (the condition) starts at the beginning of the previous row
         // (or right before the file if this is the first iteration).
-        
+
         while (_tokens.MoveNext())
         {
             var currentToken = _tokens.Current;
-            
+
             // since we remove comments from the enumerable, we don't need to check for them here
             switch (currentToken.Type)
             {
@@ -40,7 +40,7 @@ public class TimelineParser : IDisposable
                 case TimelineTokenType.Whitespace:
                 case TimelineTokenType.Equals:
                 case TimelineTokenType.Colon:
-                    // whitespace, equals, colons cannot start a directive, so bail
+                // whitespace, equals, colons cannot start a directive, so bail
                 default:
                     // TODO: provide a more descriptive error
                     throw new Exception($"Expected Identifier to start a directive, received {currentToken.Type}");
@@ -59,22 +59,22 @@ public class TimelineParser : IDisposable
                 throw new Exception($"Expected either whitespace or equals, got {_tokens.Current.Type}");
             }
         }
-        
+
         // discard the equal sign and any whitespace until we hit a non-whitespace token
-        while (_tokens.MoveNext() && _tokens.Current.Type == TimelineTokenType.Whitespace);
+        while (_tokens.MoveNext() && _tokens.Current.Type == TimelineTokenType.Whitespace) ;
 
         return _tokens.Current.Type switch
         {
             // if next non-whitespace token is an identifier, parse a single line of data.
             TimelineTokenType.Identifier => new TimelineDirective
             {
-                Identifier = directiveIdentifier.Lexeme, 
+                Identifier = directiveIdentifier.Lexeme,
                 AttributeLines = [ParseSingleLineDirectiveValue()]
             },
             // if it's a new line, parse a multi-line set of data
             TimelineTokenType.Newline => new TimelineDirective
             {
-                Identifier = directiveIdentifier.Lexeme, 
+                Identifier = directiveIdentifier.Lexeme,
                 AttributeLines = ParseMultiLineDirectiveValues()
             },
             // otherwise, bail. not what we expected.
@@ -86,7 +86,7 @@ public class TimelineParser : IDisposable
     private IList<TimelineDirectiveValue> ParseSingleLineDirectiveValue()
     {
         IList<TimelineDirectiveValue> attributes = [];
-        
+
         do
         {
             // assume the loop STARTS with an identifier as the current token
@@ -97,32 +97,31 @@ public class TimelineParser : IDisposable
             {
                 throw new Exception($"Expected colon after the identifier, got {_tokens.Current.Type}");
             }
-            
+
             // make sure we have a value next (and bail if we don't)
             if (!_tokens.MoveNext() || _tokens.Current.Type != TimelineTokenType.Identifier)
             {
                 throw new Exception($"Expected a value after the colon, got {_tokens.Current.Type}");
             }
-            
+
             var valueToken = _tokens.Current;
-            
+
             attributes.Add(new TimelineDirectiveValue
             {
                 Identifier = identifierToken.Lexeme,
                 Value = valueToken.Lexeme
             });
-            
+
             // consume whitespace until the next important token
-            while (_tokens.MoveNext() && _tokens.Current.Type == TimelineTokenType.Whitespace);
+            while (_tokens.MoveNext() && _tokens.Current.Type == TimelineTokenType.Whitespace) ;
 
             // make sure the loop starts (or the condition is checked) with an identifier or a newline
-            
+
             if (_tokens.Current.Type is not (TimelineTokenType.Identifier or TimelineTokenType.Newline))
             {
                 throw new Exception($"Expected either an identifier or a newline, got {_tokens.Current.Type}");
             }
-        }
-        while (_tokens.Current.Type != TimelineTokenType.Newline);
+        } while (_tokens.Current.Type != TimelineTokenType.Newline);
 
         return attributes;
     }
@@ -146,10 +145,10 @@ public class TimelineParser : IDisposable
             // top of the loop will consume it and make sure we're either reading another data line
             // or a new directive
         }
-        
+
         // FIXME: this leaves the current token as the beginning of the next row. works great for this loop,
         //        but the main loop assumes we start _before_ the beginning of the next row.
-        
+
         return lines;
     }
 
